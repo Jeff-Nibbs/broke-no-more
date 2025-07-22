b65595acc8659d188ec190d35b63ddaf0ea665ab
Mr. Mans <98377720+Jeff-Nibbs@users.noreply.github.com>
2025-07-21 22:42:23 -0700

    start category routes

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,5 +1,17 @@
-// server/routes/categories.js
+const express = require('express');
+const router = express.Router();
+const Categories = require('../models/Category.js');
+
 //GET /api/categories (get user's categories)
+router.get('/', async (req, res) => {});
+
+router.get('/:id', async (req, res) => {});
 //POST /api/categories (create category)
+router.post('/', async (req, res) => {});
 //PUT /api/categories/:id (update category)
+router.put('/:id', async (req, res) => {});
 //DELETE /api/categories/:id (delete category)
+router.delete('/:id', async (req, res) => {});
+
+
+module.exports = router;
